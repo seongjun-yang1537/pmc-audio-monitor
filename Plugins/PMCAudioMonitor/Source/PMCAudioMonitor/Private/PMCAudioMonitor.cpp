badcbe451edc39b92dde526efca41ef9772eb3b0
seongjun-yang1537 <seongjun.yang@5minlab.com>
2024-12-27 17:13:16 +0900

    initial project

---
+++ Plugins/PMCAudioMonitor/Source/PMCAudioMonitor/Private/PMCAudioMonitor.cpp
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PMCAudioMonitor.h"
+
+#define LOCTEXT_NAMESPACE "FPMCAudioMonitorModule"
+
+void FPMCAudioMonitorModule::StartupModule()
+{
+
+}
+
+void FPMCAudioMonitorModule::ShutdownModule()
+{
+  
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FPMCAudioMonitorModule, PMCAudioMonitor)
---
+++ Source/PMCAudioMonitorProj/PMCAudioMonitorProj.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PMCAudioMonitorProj.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PMCAudioMonitorProj, "PMCAudioMonitorProj" );
